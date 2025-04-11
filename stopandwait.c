d1b56cb0d6783544b29f40ce74aefb3c884d0498
Paravthi KB <127673281+Parubee@users.noreply.github.com>
2025-04-11 16:58:16 +0530

    Update stopandwait.c

--- stopandwait.c
+++ stopandwait.c
@@ -1,44 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
-int main() {
-    srand(time(NULL));
-    
-    int total_packets = 5;      // Total packets to send
-    int current_packet = 1;     // Current packet number
-    int last_received = 0;      // Last packet received successfully
-    
-    while (current_packet <= total_packets) {
-        // Sender sends packet
-        printf("SENDER: sent packet with seq NO:%d\n", current_packet);
-        
-        // Check if packet is received (90% chance)
-        if (rand() % 100 < 90) {
-            // Check if this is a duplicate packet
-            if (current_packet == last_received) {
-                printf("RECEIVER: Duplicated packet with seq %d\n", current_packet);
-            } else {
-                printf("RECEIVER: Received packet with seq %d\n", current_packet);
-                last_received = current_packet;
-            }
-            
-            // Try to send ACK (90% chance)
-            if (rand() % 100 < 90) {
-                printf("SENDER: Received ACK for packet %d", current_packet);
-                current_packet++;  // Move to next packet
-                if (current_packet <= total_packets) {
-                    printf("\n");  // Add newline only if not the last packet
-                }
-                printf("\n");
-            } else {
-                printf("Error While sending ACK\n\n");
-            }
-        }
-    }
-    
-    return 0;
+struct frame
+{ int info;
+  int seq;
+} ;
+int ak;
+int t=5,k;
+int disconnect=0;
+struct frame p;
+char turn='s'; //Initialize first turn as sender
+int errorframe=1; //no Error
+int errorack=1;
+
+void sender();
+void receiver();
+void main()
+{  	p.info=0;//data part
+  	 p.seq=0;//sequence number
+ 	while(!disconnect)
+ 	  {   sender(); //call sender
+     		  for(k=1;k<=10000000;k++);
+//After a finite amount of time call receiver
+receiver();
+   	}
 }
+void sender()
+{   static int flag=0;
+	if(turn=='s') //sender turn
+ 	{  if(errorack==0) //Ack didn't arrive
+  	 	{   printf("SENDER: sent packet with seq NO:%d\n",p.seq);
+  	   	 errorframe=rand()%4; //randomly pick Error frame as 4
+  	   	printf("%s\n",(errorframe==0?"Error While sending Packet":""));
+     	  	turn='r';
+	
+   		}
+  	 	else
+  		 {  
+      		if (flag==1) printf("SENDER: Received ACK for packet %d\n",ak);
+                 if (p.seq==5){  disconnect=1; return;}
+		p.info=p.info+1;
+      		 p.seq=p.seq+1;
+		printf("SENDER: sent packet with seq NO:%d\n",p.seq);
+      		 errorframe=rand()%4;
+//Message below is printed only if Error
+//occured while sending Packet
+printf("%s\n",(errorframe==0?"Error While sending Packet":""));
+      		 turn='r';
+//Set next turn as Receiver for transmission
+		flag=1;
+      
+   	 	}
+	}
+	else
+	{  	t--;
+ printf("SENDER time reducing\n");
+  		 if(t==0)
+  		  {turn='s';
+  		  errorack=0;
+  		  t=5;
+  		  }
+}
+
  
+}
+void receiver()
+{   static int frexp=1;
+    if(turn=='r')
+   {
+    if (errorframe!=0)
+	{ if(p.seq==frexp)
+//if frame sequence number is eq to frexp
+		{	printf("RECEIVER: Received packet with seq %d\n",p.seq);
+//note sequence number of frame arrived
+//to send acknowledgement
+			ak=p.seq;
+//increment the frame sequence number
+			frexp=frexp+1;
+//Set next turn as sender
+			turn='s';
+//Send acknowledgement error for frame number 4
+			errorack=rand()%4;
+			printf("%s\n",(errorack==0?"Error While sending ACK":""));
+		}
+	  else
+		{
+//Receiver received Duplicated frame for lost frame after Resending
+			printf("RECEIVER: Duplicated packet with seq %d\n",frexp-1);
+//Note down acknowledgement number of frame
+			ak=frexp-1;
+//next turn sender
+			turn='s';
+			errorack=rand()%4;
+			printf("%s\n",(errorack==0?"Error While sending ACK":""));
+		}
+         }
+}
+}
+
  
        
